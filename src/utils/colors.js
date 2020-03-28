626be22c2237c847ea3a9059b1bc6d9598a1cb68
mtkozlowski <kooziu@gmail.com>
2020-03-28 08:53:42 +0100

    update theme and colors

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,6 +1,7 @@
 export const colors = {
     primary: '#3EC2E9',
     primaryLight: '#90A660',
+    black: '#111',
     dark: '#333',
     alertLight: '#F27A5E',
     alert: '#D9483B',
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -6,9 +6,9 @@ export const theme = {
         family: "'Montserrat', sans-serif",
 
         largeHeaderSize: "94px",
+        largeHeaderSizeMobile: "72px",
         subHeaderSize: "48px",
         crumbtrailSize: "14px",
-        // blogPostTitleSize: "72px",
         blogPostTitleSize: "48px",
         blogPostTeaserSize: "24px",
         regularTextSize: "18px",
@@ -17,6 +17,7 @@ export const theme = {
         tablet: "1024px",
         tabletVertical: "768px",
         mobile: "640px",
+        mobileMid: "480px",
         mobileVertical: "360px",
     }
 }
