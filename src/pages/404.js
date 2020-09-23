667f2ce8bc7abf370ba45b53ea98d968ca32064d
mtkozlowski-dellcia <kooziu@gmail.com>
2020-09-23 13:40:28 +0200

    [feat] new 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,30 @@
+import React from "react"
+import Layout from "../style/organisms/Layout"
+import RegularSection from "../style/organisms/RegularSection"
+import MyHelmet from '../style/components/MyHelmet';
+import facebookThumbnail from './404-facebookThumbnail.jpg';
+import { Link } from 'gatsby';
+
+
+const NothingFound = () => {
+    const myHelmetData = {
+      description: "Front-end Creator on his way to UX.",
+      facebookThumbnail,
+      title: "UX & Front end",
+    }
+
+    return (
+      <>
+        <MyHelmet data={myHelmetData} />
+
+        <Layout>
+            <RegularSection>
+                <h1>404 - Nothing's here!</h1>
+                <Link to="/" >Go back to homepage</Link>
+            </RegularSection>
+        </Layout>
+      </>
+    )
+  }
+
+  export default NothingFound
