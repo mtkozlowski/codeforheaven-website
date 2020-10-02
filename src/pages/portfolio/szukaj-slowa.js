85558ead96949049b858e8a42a1dc4688f5bf51a
mtkozlowski <kooziu@gmail.com>
2020-10-02 16:09:44 +0200

    [feat] portfolio

--- src/pages/portfolio/szukaj-slowa.js
+++ src/pages/portfolio/szukaj-slowa.js
@@ -6,12 +6,16 @@ import MyHelmet from '../../style/components/MyHelmet';
 import { theme } from "../../style/theme"
 import GlobalStyle from "../../style/organisms/GlobalStyle"
 import { StyledHeader } from "../../style/organisms/Header"
+import { RegularSection, FullWidthSection } from "../../style/organisms/Sections"
+
+import szukajSlowaWebM from "./szukajSlowa/szukajSlowa.webm"
+import szukajSlowaMP4 from "./szukajSlowa/szukajSlowa.mp4"
 
 const Main = styled.main`
   margin-top: 2rem;
 `;
 
-export default function LetMeInUX() {
+export default function SzukajSlowa() {
   const myHelmetData = {
     description: "Portfolio - Lednica2000 - Mateusz Kozłowski",
     domain: "https://codeforheaven.com",
@@ -39,6 +43,15 @@ export default function LetMeInUX() {
           </Breadcrumb>
         </StyledHeader>
         <Main>
+          <RegularSection>
+          <p style={{ textAlign: "center"}}>
+                <video autoplay loop muted playsinline style={{maxWidth: "600px", display: "block", margin: "0 auto"}} >
+                  <source src={szukajSlowaWebM} type="video/webm" />
+                  <source src={szukajSlowaMP4} type="video/mp4" />
+              </video>
+              <span style={{fontSize: "0.8rem"}}>Sposób działania aplikacji</span>
+            </p>
+          </RegularSection>
         </Main>
   </ThemeProvider>
   </>
